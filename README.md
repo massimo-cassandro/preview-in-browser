43a9a017c3a20af9520c6a8360352fbf6e6bf3cd
Massimo Cassandro <massimo.cassandro@gmail.com>
2019-08-12 15:06:18 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# preview-in-browser
-VS code extension
+# preview-in-browser README
+
+VS code extension to preview a file in browser, using local web server if defined (otherwise, `file://` protocol is used).
+
+Web Server domain can be set in workspace setting.
+
+Furthermore, a specific browser can be set for previews (default: Firefox Developer Edition).
---
+++ extension.js
@@ -0,0 +1,61 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode'),
+	opener = require('opn');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('mazz.previewInBrowser', function (e) {
+    let preferences = vscode.workspace.getConfiguration('preview-in-browser');
+
+    const default_prefs = {
+      domain: null,
+      browser:null
+    };
+
+    preferences = Object.assign(default_prefs, preferences);
+
+    //console.log( preferences );
+    //console.log(  vscode.workspace.workspaceFolders[0].uri.path );
+
+    const domain = preferences.get('domain'),
+      browser = preferences.get('browser'),
+      workspacePath = vscode.workspace.workspaceFolders[0].uri.path;
+
+    let fileUrl = e.path;
+    if(workspacePath && domain) {
+        fileUrl = fileUrl.replace(workspacePath, domain +'/')
+          .replace(/\/+/g, '/');
+    }
+
+
+    // console.log('e.path: ' + e.path );
+    // console.log('fileUrl: ' + fileUrl );
+    // console.log('domain: ' + domain );
+    // console.log('browser: ' + browser );
+    // console.log('workspacePath: ' + workspacePath );
+
+
+    opener(fileUrl, browser? {app: browser} : {} );
+});
+
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
