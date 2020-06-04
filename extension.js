d9ceb1e1e7c2df07f4a9640789a0d9c7ac561f7c
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-06-04 16:04:21 +0200

    upd

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode'),
-	opener = require('opn');
+  opener = require('opn');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -29,27 +29,27 @@ function activate(context) {
 
     // console.log( preferences );
     // console.log(  vscode.workspace.workspaceFolders[0].uri.path );
-    console.log(vscode.workspace.workspaceFolders[0]);
+    // console.log(vscode.workspace.workspaceFolders);
 
-    const domain = preferences.get('domain'),
+    let domain = preferences.get('domain'),
       browser = preferences.get('browser'),
       removeStartString = preferences.get('removeStartString'),
-      workspacePath = vscode.workspace.workspaceFolders[0].uri.path;
+      fileUrl = e.path;
 
-    let fileUrl = e.path;
-    if(workspacePath && domain) {
-      fileUrl = fileUrl.replace(workspacePath + removeStartString, domain +'/')
-        .replace(/\/+/g, '/');
+    domain += /\/$/.test(domain) ? '' : '/';
+    if(/^\//.test(removeStartString)) {
+      removeStartString = removeStartString.substr(1);
     }
 
+    if(domain) {
+      fileUrl = fileUrl.replace(removeStartString, domain);
+    }
 
     console.log('e.path: ' + e.path );
     console.log('fileUrl: ' + fileUrl );
     // console.log('domain: ' + domain );
     // console.log('browser: ' + browser );
     // console.log('removeStartString: ' + removeStartString );
-    // console.log('workspacePath: ' + workspacePath );
-
 
     opener(fileUrl, browser? {app: browser} : {} );
 });
